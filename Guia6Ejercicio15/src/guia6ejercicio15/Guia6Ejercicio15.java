b51978be235d82d386c370155592233bdccaa9ea
Mario David Monroy <monroymd@hotmail.com>
2023-01-05 18:34:53 -0300

    primer commit

---
+++ Guia6Ejercicio15/src/guia6ejercicio15/Guia6Ejercicio15.java
@@ -0,0 +1,63 @@
+<<<<<<< HEAD
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia6ejercicio15;
+
+/**
+ *
+ * @author Mario
+ */
+public class Guia6Ejercicio15 {
+
+    /**
+     * @param args the command line arguments
+     * 
+     * Realizar un algoritmo que rellene un vector con los 100 primeros números enteros y los
+muestre por pantalla en orden descendente.
+     */
+    public static void main(String[] args) {
+        int [] vector = new int [100];
+        for (int i = 0; i < 100; i++) {
+            vector [i] = i + 1;
+        }
+        for (int i = 99; i >= 0; i--) {
+            System.out.println(vector[i]);
+        }
+    }
+    
+}
+=======
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia6ejercicio15;
+
+/**
+ *
+ * @author Mario
+ */
+public class Guia6Ejercicio15 {
+
+    /**
+     * @param args the command line arguments
+     * 
+     * Realizar un algoritmo que rellene un vector con los 100 primeros números enteros y los
+muestre por pantalla en orden descendente.
+     */
+    public static void main(String[] args) {
+        int [] vector = new int [100];
+        for (int i = 0; i < 100; i++) {
+            vector [i] = i + 1;
+        }
+        for (int i = 99; i >= 0; i--) {
+            System.out.println(vector[i]);
+        }
+    }
+    
+}
+>>>>>>> cd5f2a81a3ad2819b38ffd9ba43a6dab90821281
